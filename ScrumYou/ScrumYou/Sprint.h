a5fd9267c784ca7bad5d7d0c74aac1cde86feeb3
thomas.painsurget@gmail.com <thomas.painsurget@gmail.com>
2017-07-07 11:53:26 +0200

    data sprints ok

---
+++ ScrumYou/ScrumYou/Sprint.h
@@ -0,0 +1,29 @@
+//
+//  Sprint.h
+//  Scrummary
+//
+//  Created by Thomas Pain-Surget on 07/07/2017.
+//  Copyright © 2017 Bérangère La Touche. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sprint : NSObject {
+    NSString* _id_sprint;
+    NSString* _beginningDate;
+    NSString* _endDate;
+    NSString* _id_creator;
+    NSMutableArray* _id_listTasks;
+    NSMutableArray* _id_members;
+}
+
+@property (nonatomic, strong) NSString* id_sprint;
+@property (nonatomic, strong) NSString* beginningDate;
+@property (nonatomic, strong) NSString* endDate;
+@property (nonatomic, strong) NSString* id_creator;
+@property (nonatomic, strong) NSMutableArray* id_listTasks;
+@property (nonatomic, strong) NSMutableArray* id_members;
+
+- (instancetype) initWithId:(NSString*)id_sprint beginningDate:(NSString*)beginningDate endDate:(NSString*)endDate id_creator:(NSString*)id_creator id_listTasks:(NSMutableArray*)id_listTasks id_members:(NSMutableArray*)id_members;
+
+@end
